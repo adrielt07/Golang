8c2c2b19aa38cd131962d687e395ddf5abced950
adrielt07 <adriel_tolentino@outlook.com>
2019-01-30 23:27:43 +0000

    catch error if no argument is passed

--- introduction/switch_case.go
+++ introduction/switch_case.go
@@ -4,10 +4,14 @@ import "os"
 
 func main() {
 
-     switch {
-     case os.Args[1] == "1":
-     	  println("Foo")
-     case os.Args[1] == "2":
-     	  println("Bar")
+     if len(os.Args) == 1 {
+     	print("pass one Argument. 1 or 2\n")
+     } else {
+          switch {
+	  case os.Args[1] == "1":
+	       println("Foo")
+     	  case os.Args[1] == "2":
+	       println("Bar")
+     	  }
      }
 }
