6e738baf41582749192da679ff646b1430f0cc12
adrielt07 <adriel_tolentino@outlook.com>
2019-01-30 02:22:15 +0000

    initial commit

---
+++ introduction/hi_first.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+       "fmt"
+)
+
+func main() {
+     println("Hello world!")
+     fmt.Println("Hello friend!")
+}
+
+func init() {
+     println("Hi first before hello")
+}
