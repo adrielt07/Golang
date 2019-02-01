8c5bba02d9057e594a5fa8e5a39ddbf7e20ee278
adrielt07 <adriel_tolentino@outlook.com>
2019-02-01 12:09:54 -0800

    added a string that vars inst goes first then init

--- introduction/var_vs_init.go
+++ introduction/var_vs_init.go
@@ -11,5 +11,6 @@ func main() {
 // vars will run first before the init.
 // Therefore, value of message will get reassigned to "var goes first"
 func init() {
+     println("Variables declared in var block will get instantiated first")
      message = "var goes first"
 }
