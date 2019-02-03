1bd91a8b285acd9651e546b6dcccd85dcd14d040
adrielt07 <adriel_tolentino@outlook.com>
2019-02-02 17:39:00 -0800

    change the type of variable to pointer

--- introduction/FizzBuzz.go
+++ introduction/FizzBuzz.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-       "fmt"
+	"fmt"
 )
 
-func fizzbuzz(s int) {
-     for i := 0; i <= s; i++ {
-     	 if i % 15 == 0 {
-	    fmt.Printf("Fizz")
-	 } else if i % 5 == 0{
-     	    fmt.Printf("Buzz")
-	 } else if i % 3 == 0{
-     	    fmt.Printf("FizzBuzz")
-	 } else {
-	    fmt.Printf("%d", i)
-	 }
+func fizzbuzz(s *int) {
+	for i := 0; i <= *s; i++ {
+		if i%15 == 0 {
+			fmt.Printf("Fizz")
+		} else if i%5 == 0 {
+			fmt.Printf("Buzz")
+		} else if i%3 == 0 {
+			fmt.Printf("FizzBuzz")
+		} else {
+			fmt.Printf("%d", i)
+		}
 
-	 if i == s {
-	    fmt.Printf("\n")
-	 } else {
-	    fmt.Print(", ")
-	 }
-     }
+		if i == *s {
+			fmt.Printf("\n")
+		} else {
+			fmt.Print(", ")
+		}
+	}
 }
 
 func main() {
-     fizzbuzz(100)
+	var num = 100
+	var pointer *int = &num
+	fizzbuzz(pointer)
 }
