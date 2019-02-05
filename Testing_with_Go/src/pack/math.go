5367f8b5758ddcc85b52b6960873497b5b2da865
adrielt07 <adriel_tolentino@outlook.com>
2019-02-05 13:53:12 -0800

    change the function parameters and added a len check to test code coverage

--- Testing_with_Go/src/pack/math.go
+++ Testing_with_Go/src/pack/math.go
@@ -1,5 +1,26 @@
 package pack
 
-func Add(i, j int) int {
-	return 0
+/*
+Use as an example for basic testing.
+Basic testing should be 'Ok'
+
+Code coverage is not 100% on purpose.
+*/
+
+/*
+Add - adds two integers
+sum - Returns a single integers (sum)
+*/
+func Add(nums ...int) (sum int) {
+
+	// Simple example to test code coverage
+	if len(nums) == 0 {
+		println("No arguments provided")
+		return 0
+	}
+
+	for _, i := range nums {
+		sum += i
+	}
+	return
 }
