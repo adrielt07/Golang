3a564abe0cee1cf292a2f7a584df2b5502f0e42f
adrielt07 <adriel_tolentino@outlook.com>
2019-02-06 11:20:29 -0800

    added how to skip test

--- Testing_with_Go/src/pack/math_test.go
+++ Testing_with_Go/src/pack/math_test.go
@@ -10,8 +10,12 @@ import (
 )
 
 func TestCanAddNumbers(t *testing.T) {
-	result := Add(1, 2)
+	// This test will be skipped if -short flag is passed
+	if testing.Short() {
+		t.Skip("Skipping long test")
+	}
 
+	result := Add(1, 2)
 	if result != 3 {
 		// Prints the failed test
 		t.Log("Failed to add one and two")
@@ -22,5 +26,17 @@ func TestCanAddNumbers(t *testing.T) {
 	result = Add(1, 2, 3, 4)
 	if result != 10 {
 		t.Error("Failed to add more than two numebrs")
+	}
+}
+
+// This is an example of how to skip a certain test that may not be ready yet
+func TestNumbersCanFly(t *testing.T) {
+	t.Skip("Example of how to skip in testing...")
+}
+
+func TestCanMultiplyNumbers(t *testing.T) {
+	// Running test with -v flag will skip the test (Odd test)
+	if testing.Verbose() {
+		t.Skip("Not implemented yet")
 	}
 }
